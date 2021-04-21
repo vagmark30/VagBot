4d8a6c1fc0b97584d0024e211919d3b090826193
vagmark <vagmark30@gmail.com>
2021-04-22 02:47:10 +0300

    Testing entity recognition and custom actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,24 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ResolveIssue(Action):
+    def name(self) -> Text:
+        return "action_resolve_issue"
+
+    def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        contact_type = next(tracker.get_latest_entity_values('yphresia'), None)
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+        if str(contact_type) == 'apps':
+            strg = 'Για προβλήματα σχετικά με το Apps επικοινωνήστε με το noc@it.teithe.gr'
+        elif str(contact_type) == 'webmail':
+            strg = 'Για προβλήματα σχετικά με το Webmail επικοινωνήστε με το noc@it.teithe.gr ή στο https://helpdesk.the.ihu.gr'
+        elif str(contact_type) == 'thesis':
+            strg = 'Για προβλήματα σχετικά με το Thesis επικοινωνήστε με τον Κυριάκο Τσιακμάκη στο ktsiak@physics.auth.gr'
+        else:
+            strg = 'Συγγνώμη δεν κατάλαβα το πρόβλημα. Επικοινωνήστε με την γραμματεία'
+        dispatcher.utter_message(strg)     
+        return []
