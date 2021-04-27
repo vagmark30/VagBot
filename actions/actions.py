61c4834a3efca45a1009ee56deb581e4efc8e81b
vagmark <vagmark30@gmail.com>
2021-04-27 15:15:51 +0300

    Refactored naming for serviceType

--- actions/actions.py
+++ actions/actions.py
@@ -10,19 +10,19 @@ class ResolveIssue(Action):
 
     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        contact_type = next(tracker.get_latest_entity_values('yphresia'), None)
+        service_type = next(tracker.get_latest_entity_values('yphresia'), None)
 
-        if str(contact_type) == 'apps':
+        if str(service_type) == 'apps':
             strg = 'Για προβλήματα σχετικά με το Apps επικοινωνήστε με το noc@it.teithe.gr'
-        elif str(contact_type) == 'webmail':
+        elif str(service_type) == 'webmail':
             strg = 'Για προβλήματα σχετικά με το Webmail επικοινωνήστε με το noc@it.teithe.gr ή στο https://helpdesk.the.ihu.gr'
-        elif str(contact_type) == 'thesis':
+        elif str(service_type) == 'thesis':
             strg = 'Για προβλήματα σχετικά με το Thesis επικοινωνήστε με τον Κυριάκο Τσιακμάκη στο ktsiak@physics.auth.gr'
-        elif str(contact_type) == 'pithia':
+        elif str(service_type) == 'pithia':
             strg = 'Για προβλήματα σχετικά με το Πυθία επικοινωνήστε με την γραμματεία του τμήματος info@iee.ihu.gr'
-        elif str(contact_type) == 'eudoxos':
+        elif str(service_type) == 'eudoxos':
             strg = 'Για πληροφορίες σχετικά με τον Εύδοξο: γραμματεία τμήματος info@iee.ihu.gr, για τεχνικά θέματα: https://helpdesk.the.ihu.gr/, noc@the.ihu.gr'
-        elif str(contact_type) == 'moodle':
+        elif str(service_type) == 'moodle':
             strg = 'Για προβλήματα πρόσβασης στα περιεχόμενα ενος μαθήματος στο Moodle επικοινωνήστε με τον εκάστοτε υπεύθυνο καθηγητή'                  
         else:
             strg = 'Συγγνώμη δεν κατάλαβα το πρόβλημα. Μπορείτε να το αναδιατυπώσετε ή να επικοινωνήσετε με την γραμματεία του τμήματος info@iee.ihu.gr'
@@ -37,15 +37,15 @@ class InChargeofService(Action):
 
     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        contact_type = next(tracker.get_latest_entity_values('yphresia'), None)
+        service_type = next(tracker.get_latest_entity_values('yphresia'), None)
 
-        if str(contact_type) == 'apps':
+        if str(service_type) == 'apps':
             strg = 'Το apps.iee.ihu.gr και οι συνδεδεμένες εφαρμογές του, διαχειρίζονται από το τμήμα (καθηγητές και φοιτητές σε εθελοντική βάση).'
-        elif str(contact_type) == 'webmail':
+        elif str(service_type) == 'webmail':
             strg = 'Το webmail και οι συνδεδεμένες εφαρμογές τους διαχειρίζονται από το Κέντρο Δικτύου της πανεπιστημιούπολης Σίνδου (http://www.noc.teithe.gr/). '
-        elif str(contact_type) == 'moodle':
+        elif str(service_type) == 'moodle':
             strg = 'Το moodle και οι συνδεδεμένες εφαρμογές τους διαχειρίζονται από το Κέντρο Δικτύου της πανεπιστημιούπολης Σίνδου (http://www.noc.teithe.gr/). '
-        elif str(contact_type) == 'pithia':
+        elif str(service_type) == 'pithia':
             strg = 'Το pithia και οι συνδεδεμένες εφαρμογές τους διαχειρίζονται από το Κέντρο Δικτύου της πανεπιστημιούπολης Σίνδου (http://www.noc.teithe.gr/). '            
         else:
             strg = 'Συγγνώμη δεν κατάλαβα το πρόβλημα. Μπορείτε να το αναδιατυπώσετε ή να επικοινωνήσετε με την γραμματεία του τμήματος info@iee.ihu.gr'
@@ -60,21 +60,21 @@ class PasswordRecovery(Action):
 
     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        contact_type = next(tracker.get_latest_entity_values('yphresia'), None)
+        service_type = next(tracker.get_latest_entity_values('yphresia'), None)
 
-        if str(contact_type) == 'vpn':
+        if str(service_type) == 'vpn':
             strg = 'Για ανάκτηση του vpn μπορείτε να επικοινωνήσετε με γραμματεία τμήματος info@iee.ihu.gr'
-        elif str(contact_type) == 'webmail':
+        elif str(service_type) == 'webmail':
             strg = 'Για ανάκτηση του κωδικού του Webmail μπορείτε να επικοινωνήσετε με γραμματεία τμήματος info@iee.ihu.gr'
-        elif str(contact_type) == 'moodle':
+        elif str(service_type) == 'moodle':
             strg = 'Για ανάκτηση του κωδικού του moodle μπορείτε να επικοινωνήσετε με γραμματεία τμήματος info@iee.ihu.gr'
-        elif str(contact_type) == 'pithia':
+        elif str(service_type) == 'pithia':
             strg = 'Για ανάκτηση του κωδικού του pithia μπορείτε να επικοινωνήσετε με γραμματεία τμήματος info@iee.ihu.gr'
-        elif str(contact_type) == 'academicid':
+        elif str(service_type) == 'academicid':
             strg = 'Για ανάκτηση του κωδικού του academicid μπορείτε να το κάνετε αυτόματα από https://mypassword.the.ihu.gr/ ή να επικοινωνήσετε https://helpdesk.the.ihu.gr/, noc@the.ihu.gr'
-        elif str(contact_type) == 'eudoxos':
+        elif str(service_type) == 'eudoxos':
             strg = 'Για ανάκτηση του κωδικού του eudoxos μπορείτε να το κάνετε αυτόματα από https://mypassword.the.ihu.gr/ ή να επικοινωνήσετε https://helpdesk.the.ihu.gr/, noc@the.ihu.gr'
-        elif str(contact_type) == 'eduroam':
+        elif str(service_type) == 'eduroam':
             strg = 'Για ανάκτηση του κωδικού του eduroam μπορείτε να το κάνετε αυτόματα από https://mypassword.the.ihu.gr/ ή να επικοινωνήσετε https://helpdesk.the.ihu.gr/, noc@the.ihu.gr'                                    
         else:
             strg = 'Συγγνώμη δεν κατάλαβα το πρόβλημα. Μπορείτε να το αναδιατυπώσετε ή να επικοινωνήσετε με την γραμματεία του τμήματος info@iee.ihu.gr'
